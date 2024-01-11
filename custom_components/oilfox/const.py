5a9b74837502997d99998754c2de2328c0e5f1e1
chises <chises@my-chi.de>
2024-01-11 19:38:25 +0100

    Revert "code rework"

--- custom_components/oilfox/const.py
+++ custom_components/oilfox/const.py
@@ -1,9 +1,6 @@
 """Constants for the OilFox integration."""
 
 DOMAIN = "oilfox"
-
+TIMEOUT = 5
 CONF_EMAIL = "email"
 CONF_PASSWORD = "password"
-CONF_HTTP_TIMEOUT = "http-timeout"
-
-TIMEOUT = 5
--- custom_components/oilfox/sensor.py
+++ custom_components/oilfox/sensor.py
@@ -1,36 +1,37 @@
 """Platform for sensor integration."""
 
 from __future__ import annotations
-
-import logging
-
-import voluptuous as vol
-
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import SOURCE_IMPORT
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.components.sensor import (
     PLATFORM_SCHEMA,
-    SensorDeviceClass,
     SensorEntity,
+    SensorDeviceClass,
     SensorStateClass,
 )
-from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
-from homeassistant.const import (
-    EVENT_HOMEASSISTANT_START,
-    PERCENTAGE,
-    UnitOfEnergy,
-    UnitOfTime,
-    UnitOfVolume,
-)
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import callback
 from homeassistant.exceptions import ConfigEntryNotReady
-import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.device_registry import DeviceInfo
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.event import async_call_later
+from homeassistant.const import (
+    PERCENTAGE,
+    UnitOfVolume,
+    UnitOfTime,
+    EVENT_HOMEASSISTANT_START,
+    UnitOfEnergy,
+)
 from homeassistant.helpers.restore_state import RestoreEntity
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.helpers.event import async_call_later
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+)
+import logging
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 
-from .const import TIMEOUT, CONF_EMAIL, CONF_PASSWORD, CONF_HTTP_TIMEOUT, DOMAIN
+from .const import DOMAIN, CONF_EMAIL, CONF_PASSWORD
 from .OilFox import OilFox
 
 _LOGGER = logging.getLogger(__name__)
@@ -159,28 +160,18 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Initialize OilFox Integration config entry."""
-
-    _LOGGER.error("OilFox: Setup User: %s", config_entry.data[CONF_EMAIL])
+    _LOGGER.info("OilFox: Setup User: %s", config_entry.data[CONF_EMAIL])
     email = config_entry.data[CONF_EMAIL]
     password = config_entry.data[CONF_PASSWORD]
-
-    if CONF_HTTP_TIMEOUT in config_entry.options:
-        timeout = config_entry.options[CONF_HTTP_TIMEOUT]
-        _LOGGER.info(
-            "Load custom timeout value: %s", config_entry.options[CONF_HTTP_TIMEOUT]
-        )
-    else:
-        timeout = TIMEOUT
-        _LOGGER.info("Load default timeout value: %s", timeout)
-
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
-    _LOGGER.debug("OilFox Coordinator Data Result: %s", repr(coordinator.data))
+    _LOGGER.debug("OilFox Coordinator Data Result: %s", coordinator.data)
     if coordinator.data is None or coordinator.data is False:
         raise ConfigEntryNotReady(
             "Error on Coordinator Data Result: " + repr(coordinator.data)
         )
     oilfox_devices = coordinator.data["items"]
     entities = []
+    _LOGGER.debug("OilFox: Full API response: %s", oilfox_devices)
     for oilfox_device in oilfox_devices:
         _LOGGER.info("OilFox: Found Device in API: %s", oilfox_device["hwid"])
         for sensor in SENSORS.items():
@@ -203,40 +194,41 @@ async def async_setup_entry(
                     oilfox_device[sensor[0]],
                 )
                 oilfox_sensor.set_state(oilfox_device[sensor[0]])
-            elif sensor[0] == "validationError":
-                _LOGGER.debug(
-                    'Prefill entity %s with "No Error"',
-                    sensor[0],
-                )
-                oilfox_sensor.set_state("No Error")
-            elif sensor[0] == "usageCounter":
-                _LOGGER.debug(
-                    'Prefill entity %s with "0"',
-                    sensor[0],
-                )
-                oilfox_sensor.set_state(float(0))
-            elif sensor[0] == "usageCounterQuantity":
-                _LOGGER.debug(
-                    'Prefill entity %s with "0"',
-                    sensor[0],
-                )
-                oilfox_sensor.set_state(float(0))
             else:
-                _LOGGER.debug(
-                    "Prefill entity %s with empty value",
-                    sensor[0],
-                )
-            oilfox_sensor.set_state("")
+                if sensor[0] == "validationError":
+                    _LOGGER.debug(
+                        'Prefill entity %s with "No Error"',
+                        sensor[0],
+                    )
+                    oilfox_sensor.set_state("No Error")
+                elif sensor[0] == "usageCounter":
+                    _LOGGER.debug(
+                        'Prefill entity %s with "0"',
+                        sensor[0],
+                    )
+                    oilfox_sensor.set_state(float(0))
+                elif sensor[0] == "usageCounterQuantity":
+                    _LOGGER.debug(
+                        'Prefill entity %s with "0"',
+                        sensor[0],
+                    )
+                    oilfox_sensor.set_state(float(0))
+                else:
+                    _LOGGER.debug(
+                        "Prefill entity %s with empty value",
+                        sensor[0],
+                    )
+                    oilfox_sensor.set_state("")
             entities.append(oilfox_sensor)
     async_add_entities(entities)
 
 
 class OilFoxSensor(CoordinatorEntity, SensorEntity, RestoreEntity):
-    """OilFox Sensor Class."""
+    """OilFox Sensor Class"""
 
-    api_response: str = ""
-    oilfox: OilFox
-    sensor: list[SensorStateClass]
+    api_response = None
+    oilfox = None
+    sensor = None
     battery_mapping = {
         "FULL": 100,
         "GOOD": 70,
@@ -244,7 +236,7 @@ class OilFoxSensor(CoordinatorEntity, SensorEntity, RestoreEntity):
         "WARNING": 20,
         "CRITICAL": 0,
     }
-    validation_error_mapping = {
+    validationError_mapping = {
         "NO_METERING": "No measurement yet",
         "EMPTY_METERING": "Incorrect Measurement",
         "NO_EXTRACTED_VALUE": "No fill level detected",
@@ -257,14 +249,13 @@ class OilFoxSensor(CoordinatorEntity, SensorEntity, RestoreEntity):
     }
 
     def __init__(self, coordinator, element, sensor, hass):
-        """Init for OilFoxSensor."""
         super().__init__(coordinator)
         self._hass = hass
         self._attr_device_class = sensor[4]
         self.sensor = sensor
         self.oilfox = element
         self._state = None
-        self.api_response: list[SensorStateClass]
+        self.api_response = {}
         self._extra_state_attributes = {}
 
     async def async_added_to_hass(self) -> None:
@@ -275,20 +266,20 @@ class OilFoxSensor(CoordinatorEntity, SensorEntity, RestoreEntity):
         if self.sensor[0] == "usageCounter":
             if not state:
                 _LOGGER.debug("No saved State for %s", self.sensor[0])
-                self._state = 0
+                self._state = int(0)
             else:
                 _LOGGER.debug(
                     "Old State %s for %s restored", state.state, self.sensor[0]
                 )
                 self._state = state.state
             if state is None or state.attributes.get("Previous Value") is None:
-                self._extra_state_attributes["Previous Value"] = 0
+                self._extra_state_attributes["Previous Value"] = int(0)
             else:
                 self._extra_state_attributes["Previous Value"] = state.attributes.get(
                     "Previous Value"
                 )
             if state is None or state.attributes.get("Current Value") is None:
-                self._extra_state_attributes["Current Value"] = 0
+                self._extra_state_attributes["Current Value"] = int(0)
             else:
                 self._extra_state_attributes["Current Value"] = state.attributes.get(
                     "Current Value"
@@ -296,20 +287,20 @@ class OilFoxSensor(CoordinatorEntity, SensorEntity, RestoreEntity):
         if self.sensor[0] == "usageCounterQuantity":
             if not state:
                 _LOGGER.debug("No saved State for %s", self.sensor[0])
-                self._state = 0
+                self._state = int(0)
             else:
                 _LOGGER.debug(
                     "Old State %s for %s restored", state.state, self.sensor[0]
                 )
                 self._state = state.state
             if state is None or state.attributes.get("Previous Value") is None:
-                self._extra_state_attributes["Previous Value"] = 0
+                self._extra_state_attributes["Previous Value"] = int(0)
             else:
                 self._extra_state_attributes["Previous Value"] = state.attributes.get(
                     "Previous Value"
                 )
             if state is None or state.attributes.get("Current Value") is None:
-                self._extra_state_attributes["Current Value"] = 0
+                self._extra_state_attributes["Current Value"] = int(0)
             else:
                 self._extra_state_attributes["Current Value"] = state.attributes.get(
                     "Current Value"
@@ -361,7 +352,7 @@ class OilFoxSensor(CoordinatorEntity, SensorEntity, RestoreEntity):
                                 self._extra_state_attributes["Previous Value"],
                                 self._extra_state_attributes["Current Value"],
                             )
-                            self._state = 0
+                            self._state = int(0)
                         else:
                             self._extra_state_attributes[
                                 "Previous Value"
@@ -416,7 +407,7 @@ class OilFoxSensor(CoordinatorEntity, SensorEntity, RestoreEntity):
                                 self._extra_state_attributes["Previous Value"],
                                 self._extra_state_attributes["Current Value"],
                             )
-                            self._state = 0
+                            self._state = int(0)
                         else:
                             self._extra_state_attributes[
                                 "Previous Value"
@@ -457,21 +448,18 @@ class OilFoxSensor(CoordinatorEntity, SensorEntity, RestoreEntity):
 
     @property
     def extra_state_attributes(self):
-        """Get extra Attribute."""
         return self._extra_state_attributes
 
     def set_api_response(self, response):
-        """Set API response manual."""
+        """Set API response manual"""
         self.api_response = response
 
     def set_state(self, state):
-        """Set state manual."""
-        if state is not None and state != "":
-            _LOGGER.debug("Set new state %s for sensor %s", state, self.sensor[0])
-            if self.sensor[0] == "batteryLevel":
-                self._state = self.battery_mapping[state]
-            else:
-                self._state = state
+        """Set state manual"""
+        if self.sensor[0] == "batteryLevel":
+            self._state = self.battery_mapping[state]
+        else:
+            self._state = state
 
     @property
     def icon(self) -> str:
@@ -482,7 +470,9 @@ class OilFoxSensor(CoordinatorEntity, SensorEntity, RestoreEntity):
     def unique_id(self) -> str:
         """Return the unique_id of the sensor."""
 
-        """Dirty workaround: entites meteringAt did use self.sensor[3] instead of self.sensor[0] as ID before ha-oilfox version 0.1.8"""
+        """
+        Dirty workaround: entites meteringAt did use self.sensor[3] instead of self.sensor[0] as ID before ha-oilfox version 0.1.8
+        """
         if self.sensor[0] == "currentMeteringAt":
             return "OilFox-" + self.oilfox.hwid + "-" + self.sensor[3]
         if self.sensor[0] == "nextMeteringAt":
@@ -506,7 +496,7 @@ class OilFoxSensor(CoordinatorEntity, SensorEntity, RestoreEntity):
 
     @property
     def state_class(self) -> SensorStateClass:
-        """Return the state class."""
+        """Return the state class"""
         return self.sensor[5]
 
     @property
