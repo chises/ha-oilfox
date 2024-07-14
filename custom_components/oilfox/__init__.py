321b29be8f3c70dda2edb56938a2197b0aecc87c
Christoph <Christoph@Caina.de>
2024-07-14 11:56:48 +0200

    Update __init__.py
    changes to support multiple platforms (sensors, binary_sensors, ...)

--- custom_components/oilfox/__init__.py
+++ custom_components/oilfox/__init__.py
@@ -12,7 +12,7 @@ from .OilFox import OilFox
 from .UpdateCoordinator import UpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
-
+PLATFORMS = [Platform.SENSOR, Platform.BINARY_SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Setup OilFox with config entry."""  # noqa: D401
@@ -27,7 +27,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Register Handler for options flow update."""
     entry.add_update_listener(update_listener)
 
-    await hass.config_entries.async_forward_entry_setups(entry, (Platform.SENSOR,))
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
@@ -41,4 +41,4 @@ async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_forward_entry_unload(entry, PLATFORMS)
